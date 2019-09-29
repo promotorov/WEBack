f5aabedf33f46dc4d38877d50c8076f65fb1cc94
Vlad <vpromotorov@gmail.com>
2019-09-29 22:30:28 +0300

    Imlement login

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const userRouter = require('./routers/user')
+const sessionRouter = require('./routers/session')
 const connectDb = require('./models/index')
 require('dotenv').config()
 
 app.use(express.json())
 app.get('/', (req, res) => res.send('Hello world!'))
 app.use('/api/users', userRouter)
+app.use('/api/session', sessionRouter)
 
 connectDb().then(() => {
   app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
---
+++ routers/session.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const internalError = require('./common')
+const router = express.Router()
+
+// login
+router.post('/', async (req, res) => {
+  try {
+    const error = 'Username or password is incorrect'
+    const user = await User.findOne({ email: req.body.email }).select('password _id firstName lastName')
+    if (!user) {
+      res.status(422).json({ error })
+      return
+    }
+    const match = await bcrypt.compare(req.body.password, user.password)
+    if (match) {
+      const token = jwt.sign({
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName
+      }, process.env.JWT_KEY, { expiresIn: '1h' })
+      res.status(200).json({ token })
+    }
+    else
+      res.status(422).json({ error })
+  }
+  catch (error) {
+    console.log(error)
+    res.status(503).json(internalError)
+  }
+})
+
+module.exports = router
